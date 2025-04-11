1579d0ed21837f5611e49906529bb763ecb5fa00
pranto <jhpranto53@gmail.com>
2025-04-11 17:10:08 +0600

    chore: project basic setup complete

---
+++ src/app/layout.tsx
@@ -0,0 +1,32 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import "~/styles/globals.css";
+
+import { type Metadata } from "next";
+import { Geist } from "next/font/google";
+
+import { TRPCReactProvider } from "~/trpc/react";
+
+export const metadata: Metadata = {
+  title: "Code Sage",
+  description: "A tool of Github repo and PR analysis",
+  icons: [{ rel: "icon", url: "/favicon.ico" }],
+};
+
+const geist = Geist({
+  subsets: ["latin"],
+  variable: "--font-geist-sans",
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <ClerkProvider>
+      <html lang="en" className={`${geist.variable}`}>
+        <body>
+          <TRPCReactProvider>{children}</TRPCReactProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
